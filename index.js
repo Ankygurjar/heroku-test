71e30b29a0474a4af83128461b13800b4fb9fbcd
Ankygurjar <ankitchoudhary699.ac@gmail.com>
2020-11-12 11:04:16 -0500

    uploading new

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var SERVER_PORT = 5500;
+var PORT = process.env.PORT = '5500';
 app.use(express.static('client'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + './../client/index.html');
 });
-app.listen(SERVER_PORT, function () {
-    console.log("Server is up and running at port " + SERVER_PORT);
+app.listen(PORT, function () {
+    console.log("Server is up and running at port " + PORT);
 });
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const SERVER_PORT:number = 5500
+const PORT = process.env.PORT || '5500'
 
 app.use(express.static('client'))
 
@@ -10,6 +10,6 @@ app.get('/', (req:any, res:any)=>{
     res.sendFile(__dirname + './../client/index.html')
 })
 
-app.listen(SERVER_PORT, ()=>{
-    console.log(`Server is up and running at port ${SERVER_PORT}`)
+app.listen(PORT, ()=>{
+    console.log(`Server is up and running at port ${PORT}`)
 })
