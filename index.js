1bb8eff32d6054d77b52c1db053076b427265fa6
Ankygurjar <ankitchoudhary699.ac@gmail.com>
2020-11-12 14:01:43 -0500

    New Update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || '5500';
+var PORT = process.env.PORT || 5500;
+var data = require('./data.json');
 app.use(express.static('client'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + './../client/index.html');
+});
+app.get('/players', function (req, res) {
+    res.send(data);
 });
 app.listen(PORT, function () {
     console.log("Server is up and running at port " + PORT);
--- index.ts
+++ index.ts
@@ -2,12 +2,18 @@ const express = require('express')
 
 const app = express()
 
-const PORT = process.env.PORT || '5500'
+const PORT = process.env.PORT || 5500
+
+const data = require('./data.json')
 
 app.use(express.static('client'))
 
 app.get('/', (req:any, res:any)=>{
     res.sendFile(__dirname + './../client/index.html')
+})
+
+app.get('/players' ,(req:any, res:any)=>{
+    res.send(data)
 })
 
 app.listen(PORT, ()=>{
